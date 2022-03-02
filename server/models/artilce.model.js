aa4fce3bcb1cc20956d5dbe04119ef76c25db0b7
Rahul yadav <rahul.yadav96@hotmail.com>
2022-03-02 16:49:01 +0530

    fixed

--- server/models/artilce.model.js
+++ server/models/artilce.model.js
@@ -3,7 +3,7 @@ const articleSchema = new mongoose.Schema({
     img:{type:String, required:true},
     title:{type:String,required:true},
     short_Summary:{type:String,required:true},
-    content_Categery:{type:String,required:true},
+    content_Categary:{type:String,required:true},
     city:{type:String,required:true},
     auther:{type:String,required:true},
     time_to_read:{type:Number,required:true},
